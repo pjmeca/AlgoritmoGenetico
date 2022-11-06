31949b7cc1cfb8816a09b0be673ac1bd53b4d657
Pablo Meca <90964186+pjmeca@users.noreply.github.com>
2022-11-06 11:39:20 +0100

    Cuestion 3
    Cambiar los envíos de individuo_type por MPI_Pack y MPI_Unpack

--- src/ga.c
+++ src/ga.c
@@ -48,13 +48,24 @@ static int comp_fitness2(const void *a, const void *b)
 void crear_imagen(const RGB *imagen_objetivo, int num_pixels, int ancho, int alto, int max, int num_generaciones,
 	int tam_poblacion, RGB *imagen_resultado, const char *output_file, MPI_Datatype rgb_type, MPI_Datatype individuo_type)
 {
+	int name, p;
+	MPI_Comm_rank(MPI_COMM_WORLD, &name);
+	MPI_Comm_size(MPI_COMM_WORLD, &p);
+
 	// Intercambios MPI
 	int NGM = num_generaciones*NGM_PORCENTAJE;
 	int NEM = tam_poblacion>10 ? 10 : 1;
 
-	int name, p;
-	MPI_Comm_rank(MPI_COMM_WORLD, &name);
-	MPI_Comm_size(MPI_COMM_WORLD, &p);
+	// Buffers de envío de cada proceso
+	int posicion=0; //
+	int size_ind = sizeof(double)+TAM_IMAGEN*3; //
+	char *sendbuf = malloc(size_ind*NEM);
+	char *sendbufaux = malloc(size_ind*NEM*p); //
+	Individuo *sendbuf2; 
+	if(name == 0){
+		sendbuf2 = malloc(NEM*p*sizeof(Individuo));
+	}else 
+		sendbuf2 = malloc(NEM*sizeof(Individuo));
 
 	int tam_poblacion_proceso = tam_poblacion/p;
 	if (name == p-1)
@@ -108,15 +119,9 @@ void crear_imagen(const RGB *imagen_objetivo, int num_pixels, int ancho, int alt
 
 		// Envíos intermedios a P0
 		if(g%(NGM-1) == 0){
-
-			// Buffer de envío de cada proceso
-			Individuo *sendbuf = malloc(NEM*sizeof(Individuo));
-			Individuo *sendbuf2; 
-			if(name == 0)
-				sendbuf2 = malloc(NEM*p*sizeof(Individuo));
-			else 
-				sendbuf2 = malloc(NEM*sizeof(Individuo));
-
+			
+			posicion = 0;
+			
 			// Cada proceso elige NEM posiciones aleatorias y las almacena en un array
 			int* posiciones = malloc(NEM*sizeof(int));
 			for(int i=0; i<NEM; i++){
@@ -134,32 +139,68 @@ void crear_imagen(const RGB *imagen_objetivo, int num_pixels, int ancho, int alt
 				posiciones[i] = num;
 
 				// Cada proceso mueve los individuos de esas posiciones al buffer de envío
-				memmove(&sendbuf[i], poblacion[num], sizeof(Individuo));
-			}
 
-			// Cada proceso envía al proceso 0 sus individuos
-			MPI_Gather(sendbuf, NEM, individuo_type,
-					   sendbuf2, NEM, individuo_type,
-					   0, MPI_COMM_WORLD);
+				// RGB				
+				MPI_Pack(poblacion[num]->imagen, TAM_IMAGEN, rgb_type,//
+            		sendbuf, size_ind*NEM, &posicion, MPI_COMM_WORLD);//
+
+				// Fitness
+				MPI_Pack(&(poblacion[num]->fitness), 1, MPI_DOUBLE,//
+            		sendbuf, size_ind*NEM, &posicion, MPI_COMM_WORLD);//
+			}
+			
+			MPI_Gather(sendbuf, size_ind*NEM, MPI_PACKED,
+					   sendbufaux, size_ind*NEM, MPI_PACKED,
+					   0, MPI_COMM_WORLD); // 
+
+			if(name == 0){
+				posicion = 0;
+
+				for(int i=0; i<p; i++){
+					for(int j=0; j<NEM; j++) {
+						Individuo *ind = &(sendbuf2[i*NEM+j]);
+						RGB *imagen = ind->imagen;
+						MPI_Unpack(sendbufaux, size_ind*NEM*p, &posicion, imagen, TAM_IMAGEN, rgb_type, MPI_COMM_WORLD);
+						double *fitness = &(ind->fitness);
+						MPI_Unpack(sendbufaux, size_ind*NEM*p, &posicion, fitness, 1, MPI_DOUBLE, MPI_COMM_WORLD);
+					}
+				}
+			}
 
 			// El proceso 0 ordena por el fitness
 			if (name == 0)
 				qsort(sendbuf2, NEM*p, sizeof(Individuo), comp_fitness2);
 
 			// Y envía los mejores a todos los procesos
-			MPI_Bcast(sendbuf2, NEM, individuo_type, 0, MPI_COMM_WORLD);
-
-			// Cada proceso vuelve a colocar los individuos en las posiciones seleccionadas
-			for (int i=0; i<NEM; i++){
-				memmove(poblacion[posiciones[i]], &sendbuf2[i], sizeof(Individuo));
+			if(name == 0){
+				posicion = 0;
+				for(int i=0; i<NEM; i++){
+					MPI_Pack(sendbuf2[i].imagen, TAM_IMAGEN, rgb_type,
+						sendbuf, size_ind*NEM, &posicion, MPI_COMM_WORLD);
+					MPI_Pack(&(sendbuf2[i].fitness), 1, MPI_DOUBLE,
+						sendbuf, size_ind*NEM, &posicion, MPI_COMM_WORLD);
+				}
 			}
+
+			MPI_Bcast(sendbuf, size_ind*NEM, MPI_PACKED, 0, MPI_COMM_WORLD);
+
+			// Los procesos lo reciben y desempaquetan
+			//printf("A [%d, %d] Primer pixel R=%u G=%u B=%u F=%f \n", name, g, sendbuf2[0].imagen[100].r, sendbuf2[0].imagen[100].g, sendbuf2[0].imagen[100].b, sendbuf2[0].fitness);
+			// Cada proceso vuelve a colocar los individuos en las posiciones seleccionadas
+			posicion = 0;
+			Individuo *ind = malloc(sizeof(Individuo));
+			for (int i=0; i<NEM; i++){
+				MPI_Unpack(sendbuf, size_ind*NEM, &posicion, ind->imagen, TAM_IMAGEN, rgb_type, MPI_COMM_WORLD);
+				MPI_Unpack(sendbuf, size_ind*NEM, &posicion, &(ind->fitness), 1, MPI_DOUBLE, MPI_COMM_WORLD);
+
+				memmove(poblacion[posiciones[i]], ind, sizeof(Individuo));
+			}
+			free(ind);
 
 			// Antes de liberar, tenemos que esperar a que todos los procesos copien
 			MPI_Barrier(MPI_COMM_WORLD);
-
+			//printf("D [%d, %d] Primer pixel R=%u G=%u B=%u F=%f \n", name, g, poblacion[posiciones[0]]->imagen[100].r, poblacion[posiciones[0]]->imagen[100].g, poblacion[posiciones[0]]->imagen[100].b, poblacion[posiciones[0]]->fitness);
 			// Liberar la memoria
-			free(sendbuf);
-			free(sendbuf2);
 			free(posiciones);
 		}
 
@@ -209,12 +250,27 @@ void crear_imagen(const RGB *imagen_objetivo, int num_pixels, int ancho, int alt
 		mejores = malloc(sizeof(Individuo)*p);
 		assert(mejores);
 	}
-	MPI_Gather(poblacion[0], 1, individuo_type,
-		mejores, 1, individuo_type, 0, MPI_COMM_WORLD);
 
-	Individuo *resultado = malloc(sizeof(Individuo));
+	// Los procesos empaquetan su mejor individuo
+	posicion = 0;
+	MPI_Pack(poblacion[0]->imagen, TAM_IMAGEN, rgb_type,
+		sendbuf, size_ind, &posicion, MPI_COMM_WORLD);
+	MPI_Pack(&(poblacion[0]->fitness), 1, MPI_DOUBLE,
+		sendbuf, size_ind, &posicion, MPI_COMM_WORLD);
+
+	MPI_Gather(sendbuf, size_ind, MPI_PACKED,
+		sendbufaux, size_ind*p, MPI_PACKED, 0, MPI_COMM_WORLD);
+
+	RGB *resultado = malloc(num_pixels * sizeof(RGB));
 
 	if(name == 0){
+		// Desempaquetamos y movemos a los mejores
+		posicion = 0;
+		for(int i=0; i<p; i++){
+			MPI_Unpack(sendbufaux, size_ind*p, &posicion, mejores[i].imagen, TAM_IMAGEN, rgb_type, MPI_COMM_WORLD);
+			MPI_Unpack(sendbufaux, size_ind*p, &posicion, &(mejores[i].fitness), 1, MPI_DOUBLE, MPI_COMM_WORLD);
+		}
+
 		// Los ordenamos
 		qsort(mejores, p, sizeof(Individuo), comp_fitness2);
 
@@ -222,18 +278,19 @@ void crear_imagen(const RGB *imagen_objetivo, int num_pixels, int ancho, int alt
 	}
 
 	// Enviamos la mejor imagen al resto de procesos (importante para suavizar)
-	MPI_Bcast(resultado, 1, individuo_type, 0, MPI_COMM_WORLD);
+	MPI_Bcast(resultado, 1, rgb_type, 0, MPI_COMM_WORLD);
 		
 	// Devuelve Imagen Resultante
 	memmove(imagen_resultado, resultado, num_pixels * sizeof(RGB));	
 
 	MPI_Barrier(MPI_COMM_WORLD);
 
+	// Release memory
 	if (name == 0)
 		free(mejores);
 	free(resultado);
-
-	// Release memory
+	free(sendbuf);
+	free(sendbuf2);
 	for (int i = 0; i < tam_poblacion_proceso; i++)
 		free(poblacion[i]);
 	free(poblacion);
